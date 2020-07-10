5b18f7d17915b1af3f74c3b9603bb2e4e7487b5a
abrandao <andersonbrandaolustosa@gmail.com>
2020-07-10 21:24:28 +0000

    gem impressionist added

--- db/schema.rb
+++ db/schema.rb
@@ -81,12 +81,6 @@ ActiveRecord::Schema.define(version: 2020_07_10_202450) do
     t.string "slug"
   end
 
-  create_table "tags", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
